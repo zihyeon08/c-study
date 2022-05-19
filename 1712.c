e0e342c17cae65d4feed2e4a9bc53514a00b6dfd
지현 황 <jyeon4580@knu.ac.kr>
2022-05-19 22:12:57 +0900

    complete

---
+++ 1712.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c; //a 고정비용 b 가변비용 c노트북 가격
+	scanf("%d %d %d", &a, &b, &c);
+
+	if (c >= b)
+	{
+		printf("%d", a / (c - b) + 1);
+	}
+	else
+		printf("-1");
+
+	return 0;
+}
